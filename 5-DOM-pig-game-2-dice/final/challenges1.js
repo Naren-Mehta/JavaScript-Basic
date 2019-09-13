c0c20ce568757826fdb6891a794a363545a1b741
naren-mehta <deepakmehta.singh@gmail.com>
2019-09-13 22:12:40 +0530

    Working on challenges

---
+++ 5-DOM-pig-game-2-dice/final/challenges1.js
@@ -0,0 +1,111 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activePlayer, dice, gamePlaying, prevDice;
+init();
+
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    prevDice = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    document.getElementById('name-0').textContent = 'Player-1';
+    document.getElementById('name-1').textContent = 'Player-2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
+    gamePlaying = true;
+
+}
+
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    if (gamePlaying) {
+        var dice = Math.floor(Math.random() * 6) + 1;
+        var diceDOM = document.querySelector('.dice');
+        var currentDOM = document.querySelector('#current-' + activePlayer);
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+        if (prevDice === 6 && dice === prevDice) {
+            scores[activePlayer]=0;
+            document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+            nextPlayer();
+
+        }else if (dice !== 1) {
+            prevDice = dice;
+            roundScore = roundScore + dice;
+            currentDOM.textContent = roundScore;
+        } else {
+            nextPlayer();
+        }
+    }
+
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+
+
+    var input= document.querySelector('.final-score').value;
+    console.log(input)
+    var winningScore;
+    if(input){
+         winningScore=input;
+    }else{
+        winningScore=100;
+    }
+
+    if (gamePlaying) {
+        scores[activePlayer] = scores[activePlayer] + roundScore;
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        if (scores[activePlayer] >= winningScore) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            nextPlayer();
+        }
+    }
+
+
+});
+
+function nextPlayer() {
+    var currentDOM = document.querySelector('#current-' + activePlayer);
+    var diceDOM = document.querySelector('.dice');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+    activePlayer = activePlayer == 0 ? 1 : 0;
+    roundScore = 0;
+    document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+
+    currentDOM.textContent = roundScore;
+    diceDOM.style.display = 'none';
+    prevDice = 0;
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
