f244d2a56d8623b5b36e1e071f9e1051ea04a050
naren-mehta <deepakmehta.singh@gmail.com>
2019-09-13 13:10:49 +0530

    Working on Dice

--- 4-DOM-pig-game/final/app.js
+++ 4-DOM-pig-game/final/app.js
@@ -10,17 +10,31 @@ GAME RULES:
 */
 
 var scores, roundScore, activePlayer, dice;
+init();
 
-scores=[0,0];
-roundScore=0;
-activePlayer=0;
+function init(){
+    scores=[0,0];
+    roundScore=0;
+    activePlayer=0;
+    
+    document.querySelector('.dice').style.display='none';
+    document.getElementById('score-0').textContent=0;
+    document.getElementById('score-1').textContent=0;
+    
+    document.getElementById('current-0').textContent=0;
+    document.getElementById('current-1').textContent=0;
 
-document.querySelector('.dice').style.display='none';
-document.getElementById('score-0').textContent=0;
-document.getElementById('score-1').textContent=0;
+    document.getElementById('name-0').textContent='Player-1';
+    document.getElementById('name-1').textContent='Player-2';
 
-document.getElementById('current-0').textContent=0;
-document.getElementById('current-1').textContent=0;
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.add('active');
+
+
+}
+
+
 
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
@@ -45,19 +59,35 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
 
 
 document.querySelector('.btn-hold').addEventListener('click',function(){
+   
+
+    scores[activePlayer]=scores[activePlayer]+roundScore;   
+    document.querySelector('#score-'+activePlayer).textContent=scores[activePlayer];
+
+    if(scores[activePlayer]>=20){
+        console.log("==="+scores[activePlayer])
+        document.querySelector('#name-'+activePlayer).textContent='Winner!';
+        document.querySelector('.dice').style.display='none';
+        document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+     }else{
+        nextPlayer();
+    }
+
+   
+
+});
+
+function nextPlayer(){
     var currentDOM=document.querySelector('#current-'+activePlayer);
     var diceDOM= document.querySelector('.dice');
-
-    scores[activePlayer]=scores[activePlayer]+roundScore;
-    document.querySelector('#score-'+activePlayer).textContent=scores[activePlayer];
     document.querySelector('.player-'+activePlayer+'-panel').classList.toggle('active');
-
     activePlayer=activePlayer == 0? 1: 0;
     roundScore=0;
     document.querySelector('.player-'+activePlayer+'-panel').classList.toggle('active');
 
     currentDOM.textContent=roundScore;
     diceDOM.style.display='none';
+}
 
-});
-
+document.querySelector('.btn-new').addEventListener('click',init);
